f72defdfff3beab614d996679ee48411143d19cb
Jina Strickland <jina30@gmail.com>
2020-07-06 18:51:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
   {
-    {:home
+    :home
       {:team_name => "Brooklyn Nets",
         :colors => ["Black", "White"],
         :players => [
@@ -61,8 +61,8 @@ def game_hash
           :slam_dunks => 1
           }
         ]
-      }},
-    {:away
+      },
+    :away
       {:team_name => "Charlotte Hornets",
         :colors => ["Turquoise", "Purple"],
         :players => [
@@ -122,6 +122,6 @@ def game_hash
           :slam_dunks => 12
           }
         ]
-    }}
+    }
   }
 end
